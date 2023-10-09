4a7dc4ccadce1350f9ff2e2a00ab32b1e04f1a46
Tom Goedemé <flufsor@gmail.com>
2023-10-09 11:20:49 +0200

    add base64 encoding for pem, add DN instead of CN

--- certscanner/main.py
+++ certscanner/main.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-
+import base64
 import csv
 import socket
 import ssl
@@ -12,23 +12,29 @@ def scan_certificate(hostname, port=443):
             s.connect((hostname, 443))
             cert = s.getpeercert()
 
+        subject_dn = dict(x[0] for x in cert['subject'])
+        issuer_dn = dict(x[0] for x in cert['issuer'])
+
         subject_cn = dict(x[0] for x in cert['subject']).get('commonName', '')
         issuer_cn = dict(x[0] for x in cert['issuer']).get('commonName', '')
+
         serial_number = cert['serialNumber']
         not_before = cert['notBefore']
         not_after = cert['notAfter']
 
         certificate_pem = ssl.get_server_certificate((hostname, port))
+        certificate_pem_base64 = base64.b64encode(certificate_pem.encode()).decode('utf-8')  
 
-        return (subject_cn, issuer_cn, serial_number, not_before, not_after, certificate_pem)
+        return (subject_dn, issuer_dn, serial_number, not_before, not_after, certificate_pem_base64)
 
     except Exception as e:
        print(f"Error: {e}")
        return None
 
+
 def main(hosts_file):
     with open('certificates.csv', 'w', newline='') as csvfile:
-        fieldnames = ['Subject CN', 'Issuer CN', 'Serial Number', 'Not Before', 'Not After', 'Certificate PEM']
+        fieldnames = ['Subject DN', 'Issuer DN', 'Serial Number', 'Not Before', 'Not After', 'Certificate PEM in BASE64']
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
         writer.writeheader()
 
@@ -37,15 +43,14 @@ def main(hosts_file):
             for row in reader:
                 result = scan_certificate(row["host"], row["port"])
                 if result:
-                    subject_cn, issuer_cn, serial_number, not_before, not_after, certificate_pem = result
-                    certificate_pem = certificate_pem.replace('\n', '')
+                    subject_dn, issuer_dn, serial_number, not_before, not_after, certificate_pem_base64 = result
                     writer.writerow({
-                        'Subject CN': subject_cn,
-                        'Issuer CN': issuer_cn,
+                        'Subject DN': subject_dn,
+                        'Issuer DN': issuer_dn,
                         'Serial Number': serial_number,
                         'Not Before': not_before,
                         'Not After': not_after,
-                        'Certificate PEM': certificate_pem
+                        'Certificate PEM in BASE64': certificate_pem_base64
                     })
 
 if __name__ == '__main__':
