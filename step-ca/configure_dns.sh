b6b688e50b4e39eec965e1263aed4abe1b15e6f5
Niels Van De Ginste <niels.vandeginste@student.ap.be>
2023-10-16 18:05:44 +0200

    refactor(automation): rework all scripts to work better with the .env file

--- step-ca/configure_dns.sh
+++ step-ca/configure_dns.sh
@@ -1,14 +1,26 @@
+#!/bin/bash
+
+# Load environment variables from the .env file
+set -o allexport
+source .env
+set +o allexport
+
 while true; do
-    if docker ps --filter "name=dns.pki.project" --filter "status=running" | grep -q "dns.pki.project"; then
+    # Check if the DNS container is running with the specified name and in a running state
+    if docker ps --filter "name=dns.$PKI_DOMAIN_NAME" --filter "status=running" | grep -q "dns.$PKI_DOMAIN_NAME"; then
 
-        docker exec dns.pki.project /bin/bash -c "sh /media/setup_dns.sh"
+        # Execute a command inside the DNS container to run the 'setup_dns.sh' script
+        docker exec dns.$PKI_DOMAIN_NAME /bin/bash -c "sh /media/setup_dns.sh"
 
-        IP_DNS=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' dns.pki.project)
-        docker exec --user root -e IP_DNS="$IP_DNS" ca.pki.project bash -c 'echo -e "options rotate\noptions timeout:1\nnameserver 127.0.0.11\nnameserver $IP_DNS" | tee /etc/resolv.conf'
+        # Obtain the IP address of the DNS container
+        IP_DNS=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' dns.$PKI_DOMAIN_NAME)
+
+        # Update the '/etc/resolv.conf' file inside the CA container to use the DNS container's IP address
+        docker exec --user root -e IP_DNS="$IP_DNS" ca.$PKI_DOMAIN_NAME bash -c 'echo -e "options rotate\noptions timeout:1\nnameserver 127.0.0.11\nnameserver $IP_DNS" | tee /etc/resolv.conf'
 
         break
     else
-        echo "Wachten tot Container A is gestart..."
-        sleep 5  # Wacht 5 seconden voordat je opnieuw controleert
+        echo "Waiting for Container A to start..." # Display a message while waiting
+        sleep 5
     fi
 done
--- step-ca/create-ca.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-mkdir ./step-ca-data
-docker compose up
